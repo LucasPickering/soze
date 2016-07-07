1b5382831b2dd85d7f520d3e3b6ece3e46bbbcef
Lucas Pickering <pickering.l@husky.neu.edu>
2016-07-06 22:10:58 -0400

    Didn't need that either

--- src/me/lucaspickering/casecontrol/command/caseled/fade/CommandFadeSetSave.java
+++ src/me/lucaspickering/casecontrol/command/caseled/fade/CommandFadeSetSave.java
@@ -24,7 +24,6 @@ public class CommandFadeSetSave extends AbstractCommand {
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean execute(String[] args) {
 		final Data data = CaseControl.getData();
 		if (data.caseFadeColors.isEmpty()) {
