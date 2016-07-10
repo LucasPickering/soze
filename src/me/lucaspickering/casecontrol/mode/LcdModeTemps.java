a5450c99c39589de9962198dd8a923541553fa87
Lucas Pickering <pickering.l@husky.neu.edu>
2016-07-09 21:14:39 -0400

    Very important change

--- src/me/lucaspickering/casecontrol/mode/LcdModeTemps.java
+++ src/me/lucaspickering/casecontrol/mode/LcdModeTemps.java
@@ -8,7 +8,7 @@ public final class LcdModeTemps extends AbstractLcdMode {
 	public String[] getText() {
 		final int[] data = Funcs.getSpeedFanData();
 
-		// /u00df is the degree symbol
+		// \u00df is the degree symbol
 		text[0] = String.format("Fan: %d RPM", data[5]);
 		text[1] = String.format("CPU: %d\u00dfC %d\u00dfC", data[0], data[1]);
 		text[2] = String.format("     %d\u00dfC %d\u00dfC", data[2], data[3]);
