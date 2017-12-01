ce461ff164a1a1e8c40416ca896e43044bb74f24
Lucas Pickering <lucas@lucaspickering.me>
2017-11-30 18:21:56 -0800

    Add deploy script

---
+++ deploy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dirs', nargs='+', help="Directories to sync")
+    parser.add_argument('host', help="Destination host")
+    parser.add_argument('--target-dir', '-t', default='case-control', help="Destination directory")
+    args = parser.parse_args()
+
+    curdir = os.path.dirname(os.path.realpath(__file__))
+    gitignore_path = os.path.join(curdir, '.gitignore')
+
+    cmd = [
+        'rsync', '-av',
+        f'--exclude-from={gitignore_path}',  # Exclude files in .gitignore
+        *args.dirs,
+        f'{args.host}:{args.target_dir}'
+    ]
+
+    print(' '.join(cmd))
+    subprocess.call(cmd)
