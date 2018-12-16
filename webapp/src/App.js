048e8ae57706e66cc4fb9585cd15828bde30e2b5
Lucas Pickering <lucas@lucaspickering.me>
2018-12-16 14:15:04 -0500

    webapp: basic home page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import SozeContext from './Context';
+import classes from './App.module.scss';
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  // Load data from the API (on mount only)
+  useEffect(() => {
+    setLoading(true);
+    fetch('tyranitar:5000').then(response => {
+      setLoading(false);
+      setData(response.json());
+    });
+    // setTimeout(() => {
+    //   setLoading(false);
+    //   setData({
+    //     lcd: { color: '0x000000', link_to_led: false, mode: 'off' },
+    //     led: {
+    //       fade: { colors: [], fade_time: 5.0, saved: {} },
+    //       mode: 'off',
+    //       static: { color: '0x000000' },
+    //     },
+    //   });
+    // }, 3000);
+  }, []);
+
+  return (
+    <div className={classes.app}>
+      <SozeContext.Provider value={data}>
+        {loading ? <p>Loading...</p> : <div />}
+      </SozeContext.Provider>
+    </div>
+  );
+};
 
 export default App;
