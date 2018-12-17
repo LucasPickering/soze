e98adba36e9d24ff63a5943de23169454bf9692a
Lucas Pickering <lucas@lucaspickering.me>
2018-12-17 18:19:04 -0500

    API: Convert error response to JSON

--- server/soze_server/core/api.py
+++ server/soze_server/core/api.py
@@ -6,21 +6,22 @@ from . import settings
 app = Flask(__name__)
 
 
+def to_json(data):
+    if "pretty" in request.args:
+        return json.dumps(data, indent=4) + "\n"
+    return json.dumps(data)
+
+
 @app.route("/", defaults={"path": ""}, methods=["GET", "POST"])
 @app.route("/<path:path>", methods=["GET", "POST"])
 def route(path):
-    def to_json(data):
-        if "pretty" in request.args:
-            return json.dumps(data, indent=4) + "\n"
-        return json.dumps(data)
-
     try:
         if request.method == "GET":
             data = settings.get(path, serialized=True)
         elif request.method == "POST":
             data = settings.set(path, request.get_json())
     except (KeyError, ValueError) as e:
-        return str(e), 400
+        return to_json({"detail": str(e)}), 400
     return to_json(data)
 
 
