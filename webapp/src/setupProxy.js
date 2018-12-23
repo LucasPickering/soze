da144c2b085699b9c4cda7bfff592272f78759d9
Lucas Pickering <lucas@lucaspickering.me>
2018-12-23 11:59:01 -0500

    Add webapp to docker-compose

--- webapp/src/setupProxy.js
+++ webapp/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = app => {
   app.use(
     proxy('/api', {
-      target: 'http://localhost:5000/',
+      target: process.env.SOZE_SERVER || 'http://localhost:5000/',
       pathRewrite: { '^/api': '/' },
     })
   );
