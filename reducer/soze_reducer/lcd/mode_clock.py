1c1cf0c16268d60f0a829ec18116b3a0d8e8953d
Lucas Pickering <lucas@lucaspickering.me>
2019-01-17 21:26:52 -0500

    Reducer: Fix hours on LCD clock

--- reducer/soze_reducer/lcd/mode_clock.py
+++ reducer/soze_reducer/lcd/mode_clock.py
@@ -13,7 +13,7 @@ class ClockMode(LcdMode):
     _LONG_DAY_FORMAT = "{d:%A}, {d:%B} {d.day}"
     _SHORT_DAY_FORMAT = "{d:%A}, {d:%b} {d.day}"
     _SECONDS_FORMAT = " {d:%S}"
-    _TIME_FORMAT = "{d:%l}:{d:%M}"
+    _TIME_FORMAT = "{d:%-I}:{d:%M}"
 
     def __init__(self):
         super().__init__("clock")
@@ -22,18 +22,18 @@ class ClockMode(LcdMode):
         lines = []  # This will we populated as we go along
 
         now = datetime.now()
-        day_str = ClockMode._LONG_DAY_FORMAT.format(d=now)
-        seconds_str = ClockMode._SECONDS_FORMAT.format(d=now)
+        day_str = __class__._LONG_DAY_FORMAT.format(d=now)
+        seconds_str = __class__._SECONDS_FORMAT.format(d=now)
 
         # If the line is too long, shorten the day name
-        if len(day_str) + len(seconds_str) > ClockMode._LCD_WIDTH:
-            day_str = ClockMode._SHORT_DAY_FORMAT.format(d=now)
+        if len(day_str) + len(seconds_str) > __class__._LCD_WIDTH:
+            day_str = __class__._SHORT_DAY_FORMAT.format(d=now)
 
         # Pad the day string with spaces to make it the right length
-        day_str = day_str.ljust(ClockMode._LCD_WIDTH - len(seconds_str))
+        day_str = day_str.ljust(__class__._LCD_WIDTH - len(seconds_str))
         lines.append(day_str + seconds_str)  # First line
 
-        time_str = ClockMode._TIME_FORMAT.format(d=now)
+        time_str = __class__._TIME_FORMAT.format(d=now).rjust(5)
         time_lines = helper.make_big_text(time_str)  # Rest of the fucking lines
         # Pad each line with a space then add it to the list of lines
         lines += (f" {line}" for line in time_lines)
