6ff514a776e8f0c1ebb9fa204f33ad82eb9f2918
Lucas Pickering <lucas@lucaspickering.me>
2019-11-09 20:10:44 -0500

    Improve webapp build process

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,7 +2,8 @@
 
 # This isn't part of the Dockerfile because then it would run on the RPi
 if [[ "$@" = "" || " $@ " =~ " webserver " ]]; then
-    ( cd webapp; npm run build )
+    docker-compose -f docker-compose.build.yml build
+    docker-compose -f docker-compose.build.yml push
 fi
 
 docker-compose -f docker-compose.pi.yml build $@
--- webapp/pi.Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM arm32v6/nginx:alpine
-
-# Copy in config and build files
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY build/ /app/webapp/
